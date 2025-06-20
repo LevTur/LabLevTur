b5b2e07aa81c8c4ce93ac313205163ccf9e4d4c1
Лев <levturundaev@gmail.com>
2025-06-20 15:43:11 +0400

    Add files via upload
    Лаба 5

---
+++ Lab5DM.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Введите размер матицы смежности" << endl;
+    int n;
+    cin >> n;
+    cout << "Введите матрицу смежности" << endl;
+    vector <vector <int>> matrix(n, vector <int> (n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][k] && matrix[k][j]) {
+                    matrix[i][j] = 1;
+                }
+            }
+        }
+    }
+    cout << "Вот матрица достижимости" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
